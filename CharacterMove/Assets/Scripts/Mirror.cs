e9302a39d8fda482f384af70e36121a4b3bb5b13
Yaros.S <sergeyya@playtika.com>
2020-10-14 18:01:28 +0300

    added Render texture

---
+++ CharacterMove/Assets/Scripts/Mirror.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mirror : MonoBehaviour
+{
+
+    [SerializeField] private Camera mirrorCamera;
+    [SerializeField] private MeshRenderer meshMirror;
+    
+    void Start()
+    {
+        var mat = meshMirror.materials[0];
+        var renderTexture = new RenderTexture(512,512,16,RenderTextureFormat.Default);
+        mirrorCamera.targetTexture = renderTexture;
+        mat.SetTexture("_MainTex",renderTexture);
+    }
+
+}
